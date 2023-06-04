3561bfd40848830580fa3431ad48e275730f23e8
bboiko <79659123+bboiko-CLGX@users.noreply.github.com>
2023-06-04 13:58:38 +0300

    Implement service registry

--- src/main/java/org/example/ResourceProcessorApplication.java
+++ src/main/java/org/example/ResourceProcessorApplication.java
@@ -2,11 +2,13 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.retry.annotation.EnableRetry;
 
 @SpringBootApplication
 @EnableFeignClients
+@EnableDiscoveryClient
 @EnableRetry
 public class ResourceProcessorApplication {
 
